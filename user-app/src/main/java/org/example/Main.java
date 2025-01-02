c15aa07b7aedd76762d5eb977c874a8978c0dcb8
Min Zhu <zhumin@google.com>
2025-01-02 17:48:34 -0500

    added slf4j + logback logs in user app.

--- user-app/src/main/java/org/example/Main.java
+++ user-app/src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
 
     System.out.println("This is the user app.");
     MyLibrary.testLogging();
+
+    LOGGER.info("This is log item from user app");
+    LOGGER.atInfo().addKeyValue("key", "string value").log("Another message from user app");
 
   }
 }
