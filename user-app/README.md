9538f85aa5d99e8c2b04e9de124b77fdeb4a1aaf
Min Zhu <zhumin@google.com>
2025-01-14 16:02:44 -0500

    more comments.

---
+++ user-app/README.md
@@ -0,0 +1,2 @@
+This user-app uses `jul-to-slf4j` to bridge library's JUL logs to logback.
+In addition, it configures a conversionRule rule to parse and output extra fields from JUL message.
--- user-app/src/main/java/org/example/CustomFieldConverter.java
+++ user-app/src/main/java/org/example/CustomFieldConverter.java
@@ -6,6 +6,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+// this can be moved to library module, but needs logback dependency
 public class CustomFieldConverter extends ClassicConverter {
   private static final Pattern FIELD_PATTERN = Pattern.compile("\\{(.+?)=(.+?)}");
 
